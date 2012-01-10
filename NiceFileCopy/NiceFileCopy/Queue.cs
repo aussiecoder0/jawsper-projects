1d8fade477de38e3a1f159683d9c3e1c826b041a
jawsper@gmail.com <jawsper@gmail.com@aa725750-22df-18b2-2580-202d682e9b9e>
2012-01-10 16:14:43 +0000

    NiceFileCopy: Initial Commit

---
+++ NiceFileCopy/NiceFileCopy/Queue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+
+namespace nl.jawsper.NiceFileCopy
+{
+    class Queue : Thread
+    {
+
+    }
+
+    class QueueItem
+    {
+        private FileInfo source;
+        private FileInfo destination;
+
+        public void run()
+        {
+            using(var fs = source.OpenRead())
+            using (var fd = destination.OpenWrite())
+            {
+                var buff = new byte[1024*1024];
+                while (fs.Position < fs.Length)
+                {
+                    int c = fs.Read(buff, 0, buff.Length);
+                    fd.Write(buff, 0, c);
+                }
+            }
+            destination.Attributes = source.Attributes;
+        }
+    }
+}
