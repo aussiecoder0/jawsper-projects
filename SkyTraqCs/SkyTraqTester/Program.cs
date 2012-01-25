0c488091b6d3d8a2c99f6eef11ef50b4bf76c174
jawsper@gmail.com <jawsper@gmail.com@aa725750-22df-18b2-2580-202d682e9b9e>
2012-01-25 20:09:57 +0000

    SkyTraqCs  - Added config WRITE

--- SkyTraqCs/SkyTraqTester/Program.cs
+++ SkyTraqCs/SkyTraqTester/Program.cs
@@ -14,18 +14,22 @@ namespace SkyTraqTester
             var st = new SkyTraq();
             st.OpenDevice("COM3", 38400);
             //st.SetSpeed(38400, false);
-            st.OutputDisable();
+            //st.OutputDisable();
             st.ReadSoftwareVersion();
+            //SkyTraqConfig cfg;
+            //st.ReadDataloggerConfig(out cfg);
+            //cfg.datalog_enable = 0;
+            //st.WriteDataloggerConfig(cfg);
 
             //st.ClearDatalog();
 
-            using (var fs = new FileStream(string.Format("output-{0:yyyyMMddThhmmss}.txt", DateTime.Now), FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            /*using (var fs = new FileStream(string.Format("output-{0:yyyyMMddThhmmss}.txt", DateTime.Now), FileMode.OpenOrCreate, FileAccess.ReadWrite))
             {
                 st.ExportDataLog(fs);
-            }
+            }*/
 
 
-            st.OutputEnableNMEA();
+            //st.OutputEnableNMEA();
 
             st.CloseDevice();
 
