e894fec78a11771c2463e041f286001807f40d1d
jawsper@gmail.com <jawsper@gmail.com@aa725750-22df-18b2-2580-202d682e9b9e>
2012-02-16 18:19:53 +0000

    MultiFileRename:  - Minor code cleanup

--- trunk/MultiFileRename/MultiFileRename/Form1.cs
+++ trunk/MultiFileRename/MultiFileRename/Form1.cs
@@ -14,32 +14,47 @@ namespace EpisodeRename
     public partial class Form1 : Form
     {
         private DirectoryInfo folder;
+        private Regex folderPattern;
+        private Regex filePattern;
+
         public Form1()
         {
             InitializeComponent();
 
-            folder = new DirectoryInfo(@"\\bami\Downloads\TV\");
-            txtFilePattern.Text = @"ntfs.+\.avi$";
+            SetFolder(@"Y:\TV\sb\");
+            SetPattern(@"\.(mkv|avi)$");
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             if (DialogResult.OK == folderBrowserDialog1.ShowDialog())
             {
-                folder = new DirectoryInfo(folderBrowserDialog1.SelectedPath);
+                SetFolder(folderBrowserDialog1.SelectedPath);
             }
         }
 
-        private List<FileInfo> GetFiles(DirectoryInfo dir, Regex dir_regex, Regex file_regex)
+        void SetFolder(String path)
+        {
+            folderBrowserDialog1.SelectedPath = path;
+            folder = new DirectoryInfo(path);
+            label1.Text = path;
+        }
+
+        void SetPattern(String pattern)
+        {
+            txtFilePattern.Text = pattern;
+        }
+
+        private List<FileInfo> GetFiles(DirectoryInfo dir)
         {
             var list = new List<FileInfo>();
             foreach (var d in dir.GetDirectories().OrderBy(x => x.Name))
             {
-                if (dir_regex == null || dir_regex.IsMatch(d.Name)) list.AddRange(GetFiles(d, dir_regex, file_regex));
+                if (folderPattern == null || folderPattern.IsMatch(d.Name)) list.AddRange(GetFiles(d));
             }
             foreach (var f in dir.GetFiles().OrderBy(x => x.Name))
             {
-                if (file_regex == null || file_regex.IsMatch(f.Name)) list.Add(f);
+                if (filePattern == null || filePattern.IsMatch(f.Name)) list.Add(f);
             }
             return list;//.OrderBy(x => x.Name).ToList();
         }
@@ -47,9 +62,11 @@ namespace EpisodeRename
         private void button2_Click(object sender, EventArgs e)
         {
             if (!(folder is DirectoryInfo) || !folder.Exists) return;
-            var dir_rx = txtFolderPattern.Text.Length == 0 ? null : new Regex(txtFolderPattern.Text, RegexOptions.IgnoreCase);
-            var file_rx = txtFilePattern.Text.Length == 0 ? null : new Regex(txtFilePattern.Text, RegexOptions.IgnoreCase);
-            var files = GetFiles(folder, dir_rx, file_rx);
+
+            folderPattern = txtFolderPattern.Text.Length == 0 ? null : new Regex(txtFolderPattern.Text, RegexOptions.IgnoreCase);
+            filePattern = txtFilePattern.Text.Length == 0 ? null : new Regex(txtFilePattern.Text, RegexOptions.IgnoreCase);
+
+            var files = GetFiles(folder);
             panel1.Controls.Clear();
             var y = 0;
             foreach (var f in files)
@@ -68,11 +85,14 @@ namespace EpisodeRename
         {
             foreach (var tb in panel1.Controls.OfType<TextBox>())
             {
-                var fi = tb.Tag as FileInfo;
-                var new_name = tb.Text;
-                if (fi.Name.Equals(new_name)) continue;
-                var dir = fi.DirectoryName;
-                fi.MoveTo(dir + "\\" + new_name);
+                if (tb.Tag is FileInfo)
+                {
+                    var fi = tb.Tag as FileInfo;
+                    var new_name = tb.Text;
+                    if (fi.Name.Equals(new_name)) continue;
+                    var dir = fi.DirectoryName;
+                    fi.MoveTo(dir + Path.PathSeparator + new_name);
+                }
             }
         }
     }
