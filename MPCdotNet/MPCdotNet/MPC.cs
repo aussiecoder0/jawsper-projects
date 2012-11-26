aa0e57e0ddaabe2ebf7f41fa498084cf5e883376
jawsper@gmail.com <jawsper@gmail.com@aa725750-22df-18b2-2580-202d682e9b9e>
2012-11-26 03:10:46 +0000

    MPCdotNet:  - Initial commit

---
+++ MPCdotNet/MPCdotNet/MPC.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MPCdotNet
+{
+    public class MPC
+    {
+        private ServerComponent s;
+
+        public MPC()
+        {
+            s = new ServerComponent();
+        }
+
+        public void Open(string server, int port)
+        {
+            s.Open(server,port);
+        }
+        public void Close()
+        {
+            s.Close();
+        }
+
+        public List<string[]> GetStatus()
+        {
+            return s.GetList("status");
+        }
+        public List<string[]> GetStats()
+        {
+            return s.GetList("stats");
+        }
+        public List<Output> GetOutputs()
+        {
+            var list = s.GetList("outputs");
+            var outputs = new List<Output>();
+            int id = 0; string name = "";
+            foreach (var kv in list)
+            {
+                if (kv[0] == "outputid") id = int.Parse(kv[1]);
+                else if (kv[0] == "outputname") name = kv[1];
+                else
+                {
+                    outputs.Add(new Output(id, name, kv[1] == "1"));
+                }
+            }
+            return outputs;
+        }
+        public List<string[]> GetCommands()
+        {
+            return s.GetList("commands");
+        }
+        public List<string[]> GetNotCommands()
+        {
+            return s.GetList("notcommands");
+        }
+
+        public List<string[]> GetCurrentSong()
+        {
+            return s.GetList("currentsong");
+        }
+
+        public bool SendCommand(string command, string args)
+        {
+            return SendCommand(command + " " + args);
+        }
+        public bool SendCommand( string command )
+        {
+            try
+            {
+                var response = s.GetList( command );
+                return true;
+            }
+            catch( MPCException mpe )
+            {
+                Console.WriteLine("Error: " + mpe.Message);
+                return false;
+            }
+        }
+    }
+}
---
+++ MPCdotNet/MPCdotNet/ServerComponent.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace MPCdotNet
+{
+    internal class ServerComponent
+    {
+        private const int READ_BUFFER_SIZE = 1024;
+        private TcpClient client;
+        private NetworkStream stream;
+        private StreamReader r;
+        private StreamWriter w;
+
+        internal ServerComponent()
+        {
+        }
+
+        internal void Open(string server, int port)
+        {
+            // connect to the server
+            client = new TcpClient(server, port);
+            stream = client.GetStream();
+
+            // init the reader with UTF8
+            r = new StreamReader(stream, Encoding.UTF8);
+            r.BaseStream.ReadTimeout = 5000;
+
+            // init the writer with UTF8 without BOM, and \n as newline
+            w = new StreamWriter(stream, new UTF8Encoding(false));
+            w.NewLine = "\n";
+            Console.WriteLine("[RX] {0}", r.ReadLine());
+        }
+        internal void Close()
+        {
+            // say byebye to the server
+            WriteLine("close");
+            // closing this will kill the reader/writer and client
+            // this has a timeout to be able to flush the buffer
+            stream.Close(5000);
+        }
+
+        internal List<string[]> GetList(string cmd)
+        {
+            WriteLine(cmd);
+
+            var list = new List<string[]>();
+
+            while (true)
+            {
+                var line = r.ReadLine();
+                if (IsError(line)) { throw new MPCException(line); }
+                if (line == "OK") break;
+
+                var kv = line.Split(new string[] { ": " }, 2, StringSplitOptions.None);
+                if (kv.Length > 0)
+                {
+                    list.Add(kv);
+                }
+
+            }
+
+            return list;
+        }
+
+        internal void WriteLine(string s)
+        {
+            Console.WriteLine("[TX] {0}", s);
+            w.WriteLine(s);
+            w.Flush();
+        }
+
+        internal string ReadLine()
+        {
+            return r.ReadLine();
+        }
+
+        internal bool IsError(string line)
+        {
+            return line.StartsWith("ACK ");
+        }
+    }
+}
