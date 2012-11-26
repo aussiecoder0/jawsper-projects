b445c4304fc264d5fa999045297146b45450f162
jawsper@gmail.com <jawsper@gmail.com@aa725750-22df-18b2-2580-202d682e9b9e>
2012-11-26 03:10:46 +0000

    MPCdotNet:  - Initial commit

---
+++ trunk/MPCdotNet/MPCdotNet/MPCException.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MPCdotNet
+{
+
+    internal class MPCException : Exception
+    {
+        int error;
+        int command_listNum;
+        string current_command;
+        string message_text;
+
+        internal MPCException(string line)
+        {
+            var rx = new Regex(@"^ACK \[(\d+)@(\d+)\] {([^}]*)} (.+)$");
+            var m = rx.Match(line);
+            if (m.Success)
+            {
+                error = int.Parse(m.Groups[1].Value);
+                command_listNum = int.Parse(m.Groups[2].Value);
+                current_command = m.Groups[3].Value;
+                message_text = m.Groups[4].Value;
+            }
+            else
+            {
+                message_text = line;
+            }
+        }
+        public override string Message
+        {
+            get
+            {
+                return string.Format("{3}", error, command_listNum, current_command, message_text);
+            }
+        }
+    }
+
+}
---
+++ trunk/MPCdotNet/MPCdotNet/Output.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MPCdotNet
+{
+    public class Output
+    {
+        private int m_ID;
+        private string m_Name;
+        private bool m_Enabled;
+        public Output(int id, string name, bool enabled)
+        {
+            m_ID = id;
+            m_Name = name;
+            m_Enabled = enabled;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Output {{ id: {0}; name: \"{1}\"; enabled: {2} }}", m_ID, m_Name, m_Enabled);
+        }
+    }
+}
